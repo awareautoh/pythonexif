d4f941395e0ff23cf13848c3890fec0733d9dd03
awareautoh <ouphachay@hotmail.com>
2017-08-20 11:44:17 +0700

    first commit

---
+++ r_img.py
@@ -0,0 +1,12 @@
+import PIL.Image
+img = PIL.Image.open('img.jpg')
+exif_data = img._getexif()
+
+#print output in dictionary 
+from PIL.ExifTags import TAGS
+exif = {
+    TAGS[k]: v
+    for k, v in img._getexif().items()
+    if k in TAGS
+}
+print (exif)
