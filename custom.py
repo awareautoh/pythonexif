821be303d726010067aebb7a3fe0beda048f3d0a
awareautoh <ouphachay@hotmail.com>
2018-05-26 09:37:01 +0100

    Update custom.py

--- custom.py
+++ custom.py
@@ -1,2 +1,11 @@
 # code about python will go here 
  
+Tag Descriptions
+Tags are divided into these main categories:
+
+Image: information related to the main image (IFD0 of the Exif data).
+Thumbnail: information related to the thumbnail image, if present (IFD1 of the Exif data).
+EXIF: Exif information (sub-IFD).
+GPS: GPS information (sub-IFD).
+Interoperability: Interoperability information (sub-IFD).
+MakerNote: Manufacturer specific information. There are no official published references for these tags.
